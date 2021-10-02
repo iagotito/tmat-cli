db7c18c99baabd49288e535c6d3f082942ed9b97
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2021-10-02 10:43:05 -0300

    add project configuration and authentication
    Create requirements.txt

--- rgat.py
+++ rgat.py
@@ -8,12 +8,29 @@ Options:
     --version  Show version.
 
 """
+import getpass
+
+import yaml
+
 from modules.docopt import docopt
+
+
+def create_config_file():
+    config = {}
+    config["project_url"] = input("Project url: ")
+    # TODO: use api key to avoid write login info in a file
+    config["username"] = input("Usarname: ")
+    config["password"] = getpass.getpass("Password: ")
+
+    with open("config.yaml", "w") as f:
+        yaml.dump(config, stream=f, sort_keys=False)
 
 
 def main():
     arguments = docopt(__doc__, version="RGAT 0.1")
-    print(arguments)
+
+    if arguments.get("config"):
+        create_config_file()
 
 
 if __name__ == '__main__':
